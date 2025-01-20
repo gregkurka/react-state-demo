c4fce8e50aab8c8040a313cb45bd231b4753c44c
Greg Kurka <gregkurka@gmail.com>
2025-01-20 17:12:00 -0600

    added state changes that change together

--- src/App.js
+++ src/App.js
@@ -3,23 +3,42 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [togetherCount, setTogetherCount] = useState(0);
+
+  function handleTogetherClick() {
+    setTogetherCount(togetherCount + 1);
+  }
+
   return (
     <div>
       <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
+      <SeperateButton />
+      <SeperateButton />
+      <h1>Counters that update together</h1>
+      <TogetherButton
+        togetherCount={togetherCount}
+        onClick={handleTogetherClick}
+      />
+      <TogetherButton
+        togetherCount={togetherCount}
+        onClick={handleTogetherClick}
+      />
     </div>
   );
 }
 
-function MyButton() {
+function SeperateButton() {
   const [count, setCount] = useState(0);
 
-  function handleClick() {
+  function handleClickSeparate() {
     setCount(count + 1);
   }
 
-  return <button onClick={handleClick}>Clicked {count} times</button>;
+  return <button onClick={handleClickSeparate}>Clicked {count} times</button>;
+}
+
+function TogetherButton({ togetherCount, onClick }) {
+  return <button onClick={onClick}>Clicked {togetherCount} times</button>;
 }
 
 export default App;
