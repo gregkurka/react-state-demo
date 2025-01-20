fd4e4266b281a8543a1247378b5621c6cbb19177
Greg Kurka <gregkurka@gmail.com>
2025-01-20 16:46:52 -0600

    fufilled the requirements of the tutorial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
+
+export default App;
